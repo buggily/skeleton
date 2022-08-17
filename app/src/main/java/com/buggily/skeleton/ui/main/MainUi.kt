f7c37be8716bf3df91f63de5e48fcb8c03c85bd3
buggily <58127678+buggily@users.noreply.github.com>
2022-08-16 23:46:28 -0500

    migrate to material3

--- app/src/main/java/com/buggily/skeleton/ui/main/MainUi.kt
+++ app/src/main/java/com/buggily/skeleton/ui/main/MainUi.kt
@@ -1,7 +1,7 @@
 package com.buggily.skeleton.ui.main
 
 import androidx.compose.runtime.Composable
-import com.buggily.skeleton.ui.SkeletonTheme
+import com.buggily.skeleton.ui.theme.SkeletonTheme
 import com.buggily.skeleton.ui.home.HomeScreen
 
 @Composable
---
+++ app/src/main/java/com/buggily/skeleton/ui/theme/SkeletonTheme.kt
@@ -0,0 +1,26 @@
+package com.buggily.skeleton.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SkeletonTheme(
+    content: @Composable () -> Unit,
+) {
+    val colorScheme: ColorScheme = if (isSystemInDarkTheme()) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content,
+    )
+}
