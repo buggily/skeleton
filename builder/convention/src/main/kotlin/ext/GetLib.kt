398d72fb8aee48ab6693836524cb3e5a569c8780
Lance Holsten <58127678+buggily@users.noreply.github.com>
2022-12-06 22:07:43 -0600

    reorganize builder extensions

--- builder/convention/src/main/kotlin/ext/GetLib.kt
+++ builder/convention/src/main/kotlin/ext/GetLib.kt
@@ -1,12 +1,7 @@
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionConstraint
 import org.gradle.api.provider.Provider
 
 fun VersionCatalog.getLib(
     alias: String,
 ): Provider<MinimalExternalModuleDependency> = findLibrary(alias).get()
-
-fun VersionCatalog.getVersion(
-    alias: String,
-): VersionConstraint = findVersion(alias).get()
