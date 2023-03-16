949175120ff8af61dbce64a930a896e4cf5bdf1d
Lance Holsten <58127678+buggily@users.noreply.github.com>
2023-03-16 16:04:46 -0500

    clean toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,16 +40,6 @@ dependencies {
     implementation(libs.kotlin)
     implementation(libs.kotlinx.coroutines.android)
 
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
-
-    implementation(libs.androidx.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material3.windowSizeClass)
-
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.hilt.navigation.compose)
 }
