ed574f2eb3b94901312b27cc3afb68530278a3ab
Lance Holsten <58127678+buggily@users.noreply.github.com>
2023-03-25 22:58:14 -0500

    add kotlin options

--- builder/convention/src/main/kotlin/com/buggily/skeleton/KotlinAndroid.kt
+++ builder/convention/src/main/kotlin/com/buggily/skeleton/KotlinAndroid.kt
@@ -3,6 +3,8 @@ package com.buggily.skeleton
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *>,
@@ -18,4 +20,13 @@ internal fun Project.configureKotlinAndroid(
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + listOf()
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+private fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
 }
