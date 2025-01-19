0d6cd93380cf82b9cb93c361744b973176f4565f
Lance Holsten <58127678+buggily@users.noreply.github.com>
2025-01-19 14:01:18 -0600

    accomodate ui tests

--- app/src/main/java/com/buggily/skeleton/ui/theme/SkeletonTheme.kt
+++ app/src/main/java/com/buggily/skeleton/ui/theme/SkeletonTheme.kt
@@ -1,14 +1,28 @@
 package com.buggily.skeleton.ui.theme
 
+import android.content.Context
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun SkeletonTheme(
-    colorScheme: ColorScheme,
     content: @Composable () -> Unit,
 ) {
+    val context: Context = LocalContext.current
+    val isLight: Boolean = !isSystemInDarkTheme()
+
+    val colorScheme: ColorScheme = remember(isLight) {
+        if (isLight) {
+            lightColorSchemeCompat(context)
+        } else {
+            darkColorSchemeCompat(context)
+        }
+    }
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
